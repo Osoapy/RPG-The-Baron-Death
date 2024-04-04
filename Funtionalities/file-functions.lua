7de590be8227454b18c8513beeb0333d9976cb9d
João Gabriel Vieira Silva <143756812+Osoapy@users.noreply.github.com>
2024-04-04 14:57:58 +0000

    Correcting Bugs

--- Funtionalities/file-functions.lua
+++ Funtionalities/file-functions.lua
@@ -1,6 +1,32 @@
 -- Defining what we'll export
 local main = {
     createFile = function(character)
+        local list = {}
+        local index = 1
+        local bool = 0
+        local listFile = io.open("Funtionalities/charactersList.txt", "r")
+        for line in listFile:lines() do
+            list[index] = line
+            index = index + 1
+        end
+
+        listFile:close()
+
+        for i = 1, index, 1 do
+            if list[i] == character.name .. ".txt" then
+                bool = 1
+            end
+        end
+
+        if bool == 0 then
+            list[index] = character.name .. ".txt"
+            listFile = io.open("Funtionalities/charactersList.txt", "w")
+            for i, value in ipairs(list) do
+                listFile:write(value..'\n')
+            end
+            io.close(listFile)
+        end
+
         File = io.open(character.name .. ".txt", "w")
 
         File:write(character.fieldNames[1] .. ": " .. character.name .. '\n')
@@ -10,15 +36,15 @@ local main = {
         File:write(character.fieldNames[5] .. ": " .. character.powerRestrictions .. '\n')
         File:write(character.fieldNames[6] .. ": " .. character.ability .. '\n')
         File:write(character.fieldNames[7] .. ": " .. character.additionalDetails .. '\n')
-        for i = 1, 3, 1 do
-            File:write(character.pointFields[i] .. ": " .. character.attributeNames[i] .. "\n")
-        end
+        File:write(character.pointFields[1] .. ": " .. character.pv .. "\n")
+        File:write(character.pointFields[2] .. ": " .. character.ps .. "\n")
+        File:write(character.pointFields[3] .. ": " .. character.ca .. "\n")
         for i = 1, 6, 1 do
             File:write("=] " .. character.attribute[i] .. " - " .. character.attributeNames[i] .. "\n")
         end
         File:write(character.fieldNames[8] .. ": " .. character.itens .. '\n')
         File:write(character.fieldNames[9] .. ": " .. character.demageFonts .. '\n')
-        File:write(character.fieldNames[10] .. ": " .. character.lore .. '\n')
+        File:write(character.fieldNames[10] .. ": " .. character.lore)
 
         File:close()
     end
